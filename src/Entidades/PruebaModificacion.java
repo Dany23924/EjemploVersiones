423e0224892cb7981bf018b6dca5737984e27dd1
Dany23924 <aguilardany@gmail.com>
2023-08-08 10:25:32 -0300

    Update PruebaModificacion.java
    Cree una clase para escribir

--- src/Entidades/PruebaModificacion.java
+++ src/Entidades/PruebaModificacion.java
@@ -5,6 +5,7 @@ package Entidades;
 *acá estoy probando a ver si se modifica, estoy en netbens 
 * escribiendo, vere que pasa cuando
 * grave.
+* AGREGO ALGO MAS PARA PONER UN COMENTARIO EN GITHUB DESKTOP
  */
 public class PruebaModificacion {
     
