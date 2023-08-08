7603a63be7db04fb711e412867bf6d4e284c2630
Dany23924 <aguilardany@gmail.com>
2023-08-08 10:23:31 -0300

    Create PruebaModificacion.java

---
+++ src/Entidades/PruebaModificacion.java
@@ -0,0 +1,11 @@
+
+package Entidades;
+
+/**
+*acá estoy probando a ver si se modifica, estoy en netbens 
+* escribiendo, vere que pasa cuando
+* grave.
+ */
+public class PruebaModificacion {
+    
+}
